80ed5f3302420d23bb6c08bd0db372b88a3b236a
Jennifer Kerssen <jjkerssen@gmail.com>
2018-02-07 22:32:26 -0700

    pull header and subheader arrays

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let extractStr = '';
 let headersArr = [];
+let subheadersArr = [];
 
 //Render page
 
@@ -12,27 +13,26 @@ function getDataFromWikiApi(searchTerm) {
     success: function(data) {
       let page = Object.keys(data.query.pages)[0];
       extractStr = data.query.pages[page].extract;
-      //parseWikiText(extractStr);
-      testAPICall(extractStr);
-      pullTextHeadings(extractStr);
+      renderJSON(extractStr);
     }
   };
   $.ajax(query);
 }
 
 //test API call
-function testAPICall(extractStr) {
+function renderJSON(extractStr) {
   console.log('testAPICall ran');
-  $('.extract-results').html(`
+  $('#string').html(`
     <p>${extractStr}</p>
     `);
+  pullTextHeadings(extractStr);
+  pullTextSubheadings(extractStr);
 }
 
-//Parsing test
+// Add headings to an array
 function pullTextHeadings(extractStr) {
   console.log('pullTextHeadings ran');
-  $('#test').html(extractStr);
-  let headers = $('#test').find('h2');
+  let headers = $('#string').find('h2');
   console.log(headers);
   for (let i = 0; i < headers.length; i++) {
     headersArr.push(headers[i].textContent);
@@ -41,28 +41,47 @@ function pullTextHeadings(extractStr) {
   headersArr.splice(-3);
   console.log(headersArr);
 }
+
+// Add subheadings to an array
+function pullTextSubheadings(extractStr) {
+  console.log('pullTextHeadings ran');
+  let subheaders = $('#string').find('h3');
+  console.log(subheaders);
+  for (let i = 0; i < subheaders.length; i++) {
+    subheadersArr.push(subheaders[i].textContent);
+  };
+  console.log(subheadersArr);
+}
+
+// Create an object with headings as keys
+
+// Add text strings as object values
+
+/*
+// Render headings as links
+function renderHeaderLinks(headersArr) {
+  console.log('renderHeaderLinks ran');
+  headersArr.map((item, index) => $('.contents.links').html(`
+      <li>${item[index]}</li>
+    `)
+  );
+/*
+  headersArr.map(
+  $('.extract-results').html(`
+      <li>${headersArr[0]}</li>
+    `)
+  );
+  */
+
 //let article = $(headers[0]).child();
 //console.log(article);
 
-//Render & handle disambiguation, errors (post mvp)
-//Generate random page when no input is given (post mvp)
-//https://en.wikipedia.org/wiki/Special:Random
-
-
-//Parse Wikipedia JSON to extract and store content sections in an array of objects
-//Format text to pass to Polly API
-/*
-// 1. Grab <h2> as keys, to be displayed as LI in UL 
-function pullTextHeadings(extractStr) {
-  console.log('pullTextHeadings ran');
-  const headings = extractStr.getElementsByTagName('h2')[0];
-  console.log(headings);
-}
-*/
-// 2. Add in [0] named Intro (no header in JSON)
-// 3. Grab <h3> as subhead keys
-// 4. Assign <p> text blocks as values
-// ** Ignore See Also, References, External Links
+// TO DO
+// Render a stop button (pass '' string to polly)
+// Handle content link click - pass text or '' to polly
+// Render & handle disambiguation, errors (post mvp)
+// Generate random page when no input is given (post mvp)
+// https://en.wikipedia.org/wiki/Special:Random
 // ** Pull out any unreadable tags
 
 //Submit parsed content to Polly API
@@ -96,12 +115,6 @@ function getAudioFromPollyAPI (text) {
       }
   });
 }
-
-// Render a link for each content section
-
-// Render a stop button (pass '' string to polly)
-
-// Handle content link click - pass text or '' to polly
 
 //Handle submit search
 function submitSearch() {
